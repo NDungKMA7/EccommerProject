18d4e0acfc39d55c1348c1da5adf453329dcfe05
NDungKMA7 <kma07.ntd@gmail.com>
2023-06-19 23:24:29 +0700

    init Adv controller

---
+++ EcommerceProject/Areas/Admin/Controllers/AdvsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceProject.Areas.Admin.Controllers
+{
+    public class AdvsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
