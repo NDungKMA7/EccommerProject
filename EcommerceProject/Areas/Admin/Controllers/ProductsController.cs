7b0c28431b5126dfee83d5ddfd9db61fdcf0abbf
NDungKMA7 <kma07.ntd@gmail.com>
2023-06-21 22:42:37 +0700

    init Product manager

--- EcommerceProject/Areas/Admin/Controllers/ProductsController.cs
+++ EcommerceProject/Areas/Admin/Controllers/ProductsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EcommerceProject.Models.Mapping;
+using EcommerceProject.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceProject.Areas.Admin.Controllers
 {
@@ -7,11 +11,20 @@ namespace EcommerceProject.Areas.Admin.Controllers
     [Authorize(Policy = "Seller")]
     public class ProductsController : Controller
     {
-      
-        
-        public IActionResult Index()
+
+
+        private readonly MyDbContext _context;
+        public ProductsController(MyDbContext context)
         {
-            return View("Index", "nguvc");
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int? page)
+        {
+            int _RecordPerPage = 20;
+            int _CurrentPage = page ?? 1;
+            List<ItemProduct> _ListRecord = await _context.Products.OrderByDescending(item => item.Id).ToListAsync();
+
+            return View("Index", _ListRecord.ToPagedList(_CurrentPage, _RecordPerPage));
         }
     }
 }
