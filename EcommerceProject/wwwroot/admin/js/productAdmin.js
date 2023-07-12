964b7a423d8d9aa14fe415b625558c92012e7cea
NDungKMA7 <kma07.ntd@gmail.com>
2023-07-12 09:40:38 +0700

    Thực hiện thêm ảnh cho sản phẩm và sửa lại login cho customer

--- EcommerceProject/wwwroot/admin/js/productAdmin.js
+++ EcommerceProject/wwwroot/admin/js/productAdmin.js
@@ -24,6 +24,25 @@
                         orderable: false
                     },
                     {
+                        data: 'imgSub',
+                        render: function (data, type, row) {
+                            var html = '';
+                            if (data != " ") {
+                                var elements = data.split(', ');
+                                for (var i = 0; i < elements.length; i++) {
+                                    if (elements[i] != "") {
+                                        html += '<img src="/Upload/Products/' + elements[i] + '" style="width:100px;" />';
+                                    }
+
+                                }
+                            }
+                            
+                            return html;
+                        },
+                        orderable: false
+                    },
+                 
+                    {
                         data: 'categories',
                         render: function (data, type, row) {
                             return data;
